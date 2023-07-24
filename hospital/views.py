880d83f4b73494251615dcaf8cae8038f91db7ac
rthway <rthway@gmail.com>
2023-07-24 10:59:29 +0545

    fix urls issue adding include and and add and config templates

--- hospital/views.py
+++ hospital/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def About(request):
+    return render(request,'about.html')
+
+def Home(request):
+    return render(request,'home.html')
