87d7ff1f8def1c6f66f5b3a8f959897ffdb8da44
rthway <rthway@gmail.com>
2023-07-27 14:52:22 +0545

    add login.html, create view and urls fro login and logout

--- hospital/urls.py
+++ hospital/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.Home, name='home'),
     path('about/',views.About, name='about'),
     path('contact/',views.Contact, name='contact'),
+    path('admin_login/', views.admin_login, name='admin_login'),
+    path('admin_logout/', views.admin_logout, name='admin_logout'),
 
 ]
--- hospital/views.py
+++ hospital/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login,logout
 
 # Create your views here.
 def About(request):
@@ -9,3 +11,27 @@ def Home(request):
 
 def Contact(request):
     return render(request,'contact.html')
+
+def Index(request):
+    if not request.user.is_staff:
+        return redirect('login')
+    return render(request, 'index.html')
+
+def admin_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('uname')  # This line retrieves the value of the 'uname' field from the POST data submitted in the form.
+        password = request.POST.get('pwd')  # This line retrieves the value of the 'pwd' field from the POST data submitted in the form.
+
+        user = authenticate(request, username=username, password=password) # This line uses the authenticate function to check if the provided username and password match any user in the database. If a valid user is found, the user variable will contain the corresponding user object; otherwise, it will be None.
+        if user is not None and user.is_staff: # This line checks whether the user variable is not None (i.e., a valid user object) and whether the user has the is_staff attribute set to True. The is_staff attribute is typically used to designate admin users in Django's default user model.
+            login(request, user) # This line uses the login function to log in the user. It creates a user session and associates it with the current request.
+            return redirect('home')  # Replace 'admin_home' with the URL name of your admin dashboard/homepage
+        else:
+            error_message = "Invalid credentials or insufficient permissions. Please try again."
+            return render(request, 'login.html', {'error_message': error_message})
+    else:
+        return render(request, 'login.html')
+
+def admin_logout(request):
+    logout(request)
+    return redirect('admin_login')  # Redirect to the admin login page after logout
